8ab6b0ee9366b75cac7080956ea8e1c2d8ea0e1d
Francisco J. Moreno <franciscoj.sparcoltary@gmail.com>
2020-08-26 19:02:14 -0500

    [ADD] Sidenav and header styles

--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './components/profile.component';
+import { SharedModule } from '../../modules/shared/shared.module';
 
 @NgModule({
   declarations: [ProfileComponent],
-  imports: [CommonModule, ProfileRoutingModule],
+  imports: [CommonModule, ProfileRoutingModule, SharedModule],
 })
 export class ProfileModule {}
